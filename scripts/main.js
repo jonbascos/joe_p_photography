e45037ca47097a01af290c304370777f9ea4ad5d
Jon Bascos <jon@jonbascos.com>
2021-09-02 16:47:22 -0700

    added hamburger menu for mobile view. Still a WIP

--- scripts/main.js
+++ scripts/main.js
@@ -1,1 +1,16 @@
-console.log('working')
+const hamburger = document.getElementById('mobileNav')
+const mobileNavMenu = document.getElementById('mobileNavMenu')
+const hamburgerIcon = document.querySelector('.fa-bars')
+const mobileNavOpen = document.querySelector('#mobileNavOpen')
+const navSelector = document.querySelector('#navSelector')
+
+hamburger.addEventListener('click', () => { 
+    if(mobileNavMenu.style.display === 'none') {
+        mobileNavMenu.style.display = 'flex'
+        hamburgerIcon.style.display = 'none'
+
+    } else {
+        mobileNavMenu.style.display = 'none'
+        hamburgerIcon.style.display = 'block'
+    }
+})
