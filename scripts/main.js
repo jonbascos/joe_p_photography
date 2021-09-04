540bc09b0e025c8a9dc28a770904a01db92a7f4d
Jon Bascos <jon@jonbascos.com>
2021-09-03 22:40:49 -0700

    fixed the hamburger menu to require only one click to activate instead of two.

--- scripts/main.js
+++ scripts/main.js
@@ -1,16 +1,9 @@
-const hamburger = document.getElementById('mobileNav')
-const mobileNavMenu = document.getElementById('mobileNavMenu')
-const hamburgerIcon = document.querySelector('.fa-bars')
-const mobileNavOpen = document.querySelector('#mobileNavOpen')
-const navSelector = document.querySelector('#navSelector')
+const hamburger = document.querySelector('#hamburger')
+const navUL = document.querySelector('.nav-ul')
+
+
+console.log('JS loaded')
 
 hamburger.addEventListener('click', () => { 
-    if(mobileNavMenu.style.display === 'none') {
-        mobileNavMenu.style.display = 'flex'
-        hamburgerIcon.style.display = 'none'
-
-    } else {
-        mobileNavMenu.style.display = 'none'
-        hamburgerIcon.style.display = 'block'
-    }
+    navUL.classList.toggle('show')
 })
